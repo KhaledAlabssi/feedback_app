1c663e40d5e6741a8386168df50abdc5a6194c85
KhaledAlabssi <khaled.absi5@gmail.com>
2021-12-16 20:37:06 +0100

    edit feedback event

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -4,11 +4,25 @@ import {v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([{
-        id: 1,
-        text: 'this is default from context',
-        rating: 10
-    }])
+    const [feedback, setFeedback] = useState([
+        {
+            id: 1,
+            text: 'this is default from context',
+            rating: 6
+        }, {
+            id: 3,
+            text: 'this is default from context',
+            rating: 10
+        }, {
+            id: 2,
+            text: 'this is default from context',
+            rating: 5
+        }
+    ])
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    })
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter(item => item.id !== id))
@@ -18,8 +32,15 @@ export const FeedbackProvider = ({children}) => {
         newFeedback.id = uuidv4()
         setFeedback([newFeedback, ...feedback])
     }
+    // Set item to be updated
+    const editFeedback = (item) => {
+        setFeedbackEdit({
+            item,
+            edit: true
+        })
+    }
 
-    return <FeedbackContext.Provider value={{feedback, deleteFeedback, addFeedback}}>
+    return <FeedbackContext.Provider value={{feedback, deleteFeedback, addFeedback, editFeedback}}>
         {children}
     </FeedbackContext.Provider>
 
